29bd1291a7dc630c933263a50989566859381d1d
“Narendra <narendranag@gmail.com>
2021-08-07 20:55:03 +0530

    Fixed bug — wasn't casting the input as an int

--- collatz-conjecture.py
+++ collatz-conjecture.py
@@ -1,5 +1,8 @@
 # The Collatz Conjecture
 # https://www.youtube.com/watch?v=094y1Z2wpJg
+#
+# Narendra Nag
+# Aug 7, 2021
 
 # for any input n
 # while n != 1
@@ -18,25 +21,28 @@ counter = 0
 x_axis = [0]
 y_axis = [0]
 
-n = input("Enter seed number to start with: ")
+n = int(input("Enter seed number to start with: "))
 
-while n > 1:
+while n != 1:
 
     counter = counter + 1
     
     x_axis.append(counter)
     y_axis.append(n)
 
+    print(n)
+    
     if(n % 2 == 0):
         n = n / 2
     else:
         n = (n * 3) + 1
 
+    
 
 # plot n on graph
 
-plt.plot(x_axis, y_axis, color="blue", marker="o", linestyle="dashed", linewidth=1, markerfacecolor="green", markersize=10)
-plt.xlabel("Iteration")
-plt.ylabel("n")
-plt.title("Collatz Conjecture Series")
-plt.show()
+#plt.plot(x_axis, y_axis, color="blue", marker="o", linestyle="dashed", linewidth=1, markerfacecolor="green", markersize=10)
+#plt.xlabel("Iteration")
+#plt.ylabel("n")
+#plt.title("Collatz Conjecture Series")
+#plt.show()
