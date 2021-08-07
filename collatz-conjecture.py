3c0f6125ec9ea562c2bc8412907c06fae377cb12
“Narendra <narendranag@gmail.com>
2021-08-07 20:50:59 +0530

    Basic Collatz Conjecture Code

---
+++ collatz-conjecture.py
@@ -0,0 +1,42 @@
+# The Collatz Conjecture
+# https://www.youtube.com/watch?v=094y1Z2wpJg
+
+# for any input n
+# while n != 1
+#   if n is odd, then n = (n * 3) + 1
+#   if n is even then n = n / 2
+#   add n to the y-axis list
+#   add the iterator to the x-axis list
+# take the leading digit of n and create a histogram
+
+
+import matplotlib.pyplot as plt
+
+n = 0
+counter = 0
+
+x_axis = [0]
+y_axis = [0]
+
+n = input("Enter seed number to start with: ")
+
+while n > 1:
+
+    counter = counter + 1
+    
+    x_axis.append(counter)
+    y_axis.append(n)
+
+    if(n % 2 == 0):
+        n = n / 2
+    else:
+        n = (n * 3) + 1
+
+
+# plot n on graph
+
+plt.plot(x_axis, y_axis, color="blue", marker="o", linestyle="dashed", linewidth=1, markerfacecolor="green", markersize=10)
+plt.xlabel("Iteration")
+plt.ylabel("n")
+plt.title("Collatz Conjecture Series")
+plt.show()
