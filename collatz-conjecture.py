1538753256bdc7be30d1df9971ad6f439050c4f7
“Narendra <narendranag@gmail.com>
2021-08-07 20:59:14 +0530

    Fixed plot title to show beginning seed term, and made sure we start the series from the seed term

--- collatz-conjecture.py
+++ collatz-conjecture.py
@@ -18,17 +18,13 @@ import matplotlib.pyplot as plt
 n = 0
 counter = 0
 
-x_axis = [0]
-y_axis = [0]
 
 n = int(input("Enter seed number to start with: "))
 
-while n != 1:
+x_axis = [counter]
+y_axis = [n]
 
-    counter = counter + 1
-    
-    x_axis.append(counter)
-    y_axis.append(n)
+while n != 1:
 
     print(n)
 
@@ -36,6 +32,12 @@ while n != 1:
         n = n / 2
     else:
         n = (n * 3) + 1
+    
+    counter = counter + 1
+    
+    x_axis.append(counter)
+    y_axis.append(n)
+
 
     
 
@@ -44,5 +46,5 @@ while n != 1:
 plt.plot(x_axis, y_axis, color="blue", marker="o", linestyle="dashed", linewidth=1, markerfacecolor="green", markersize=10)
 plt.xlabel("Iteration")
 plt.ylabel("n")
-plt.title("Collatz Conjecture Series")
+plt.title("Collatz Conjecture Series for " + str(y_axis[0]))
 plt.show()
